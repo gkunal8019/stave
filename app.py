4200afbca89c4f0d7e96abddd7689c6b285724b1
KUNAL GAWANDE <113965073+gkunal8019@users.noreply.github.com>
2024-03-01 11:50:41 +0530

    new vector

--- app.py
+++ app.py
@@ -8,15 +8,14 @@ from langchain.chains.question_answering import load_qa_chain
 import os
 import openai
 
-os.environ["OPENAI_API_KEY"] = "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"
-openai.api_key = "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"
-
+os.environ["OPENAI_API_KEY"] = "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"
+openai.api_key = "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"
 
 def blog(query):
     prompt=f"go through all the doccument give the corrected answer : {query}"
     openai = ChatOpenAI(model_name="gpt-4",temperature=0.7,max_tokens=4000) # 'ada' 'gpt-3.5-turbo' 'gpt-4'
     embeddings = OpenAIEmbeddings()
-    new_db = FAISS.load_local("faiss_index_new-20240224T101307Z-001", embeddings)
+    new_db = FAISS.load_local("faiss_index_steave", embeddings)
     chain = load_qa_chain(openai, chain_type="stuff")
     docs = new_db.similarity_search(query,10)
     response=chain.run(input_documents=docs, question=prompt)
